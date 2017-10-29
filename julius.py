12dae080c55922081167329b3510c582cc8a00d9
fishkiller252 <fishkiller.1017@gmail.com>
2017-10-29 19:55:23 +0900

    Update julius.py

--- julius.py
+++ julius.py
@@ -4,7 +4,7 @@ import subprocess
 
 p = subprocess.Popen(["bash stat_julius.sh"],stdout=subprocess.PIPE,shell=True)
 pid = p.stdout.read() # juliusのプロセスIDを取得
-:
-:
+
+
 p.kill() #起動スクリプトのプロセス終了
 subprocess.call(["kill" +pid],shell=True) #juliusのプロセスを終了
