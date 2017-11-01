4f737527f62c32ba88469fb682aabf05ffa08f85
Teru222 <b1015083@fun.ac.jp>
2017-11-01 17:06:03 +0900

    Revert "update"
    This reverts commit 5146cf941ac82e5286e46464d2823c228a4f2b7b.

--- stat_julius.sh
+++ stat_julius.sh
@@ -1,5 +1,11 @@
-#!/bin/sh                                                                        
+#!/bin/sh
 
+sudo modprobe snd-pcm-oss
+sudo sh -c "echo snd-pcm-oss >> /etc/modules"
 export AUDIODEV=/dev/dsp1
 
-julius -C ~/julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/main.jconf -C ~/julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/am-gmm.jconf -nostrip
+#julius -C ~/julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/main.jconf -C ~/julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/am-gmm.jconf -nostrip
+
+julius -C ~/julius-4.4.2/julius-kits/dictation-kit-v4.3.1-linux/word.jconf -module > /dev/null &
+echo $!
+sleep 3
