661d5cd527d6312c5b7f5a28dd21a091e6e5c353
Teru222 <b1015083@fun.ac.jp>
2017-11-01 16:59:40 +0900

    update

---
+++ #start_wav.py#
@@ -0,0 +1,3 @@
+import os
+
+os.system("aplay '/home/pi/Music/ohayo.wav'")
--- wav_output.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import wave
-
-w = wave.Wave_write("/home/pi/Music/ohayo.wav")
-w.setnchannels(1)
-w.setsampwidth(2)
-w.setframerate(44100)
-w.writeframes(x)
-w.close()
