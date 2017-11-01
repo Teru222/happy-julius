bd7751f61c1ec3a4e0320d982e10976d2eaebd37
Teru222 <b1015083@fun.ac.jp>
2017-11-01 15:53:20 +0900

    Upload

--- julius.py
+++ julius.py
@@ -1,10 +1,7 @@
 import subprocess
 
-#julius起動スクリプト実行
-
 p = subprocess.Popen(["bash stat_julius.sh"],stdout=subprocess.PIPE,shell=True)
-pid = p.stdout.read() # juliusのプロセスIDを取得
+pid = p.stdout.read() 
 
-
-p.kill() #起動スクリプトのプロセス終了
-subprocess.call(["kill" +pid],shell=True) #juliusのプロセスを終了
+p.kill()
+subprocess.call(["kill" +pid],shell=True)
